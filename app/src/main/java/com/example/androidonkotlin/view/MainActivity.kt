2eb1e5046db15ba81f5693c7238f5246c1c581c3
Melsen <melsen@yandex.ru>
2021-07-11 18:03:57 +0300

    Выполнил ДЗ№3

--- app/src/main/java/com/example/androidonkotlin/view/MainActivity.kt
+++ app/src/main/java/com/example/androidonkotlin/view/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.androidonkotlin.view
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.androidonkotlin.R
+import com.example.androidonkotlin.view.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,8 +12,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
-                    .replace(R.id.container, MainFragment.newInstance())
-                    .commitNow()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
         }
     }
 }
